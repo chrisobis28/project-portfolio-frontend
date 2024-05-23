54311e21ea30c3020531aed7b6a17923e22744a3
lsionescu <ionescu-8@student.tudelft.nl>
2024-05-23 14:13:29 +0200

    added all classes from commons into the frontend

--- src/app/interface/Classes.ts
+++ src/app/interface/Classes.ts
@@ -4,4 +4,100 @@ export interface Project {
     description: string;
     bibtex: string
     archived: boolean
+    media: Media[]
+    projectsToAccounts: ProjectsToAccounts[]
+    projectsToCollaborators: ProjectsToCollaborators[]
+    tagsToProjects: TagToProject[]
+    links: Link[]
+    requests: Request[]
+
 }
+
+export interface Media {
+    mediaId: string
+    name: string
+    path: string
+    requestMediaProjects: RequestMediaProject[]
+}
+
+export interface ProjectsToAccounts {
+    ptaId: string
+    role: string
+}
+
+export interface Account {
+    username: string
+    name: string
+    password: string
+    isAdministrator: boolean
+    isPM: boolean
+    projectstoAccounts: ProjectsToAccounts[]
+    requests: Request[]
+}
+
+export interface ProjectsToCollaborators {
+    ptcId: string
+    role: string
+}
+
+export interface TagToProject {
+    tagToProjectId: string
+}
+
+export interface Link {
+    linkId: string
+    name: string
+    url: string
+    requestLinkProjects: RequestLinkProject[]
+}
+
+export interface RequestMediaProject {
+    requestMediaProjectId: string
+    isRemove: boolean
+}
+
+export interface RequestLinkProject {
+    requestLinkProjectId: string
+    isRemove: boolean
+
+}
+
+export interface Collaborator {
+    collaboratorId: string
+    name: string
+    projectsToCollaborators: ProjectsToCollaborators[]
+    requestCollaboratorsProjects: RequestCollaboratorsProjects[]
+}
+
+export interface Request {
+    requestId: string
+    newTitle: string
+    newDescription: string
+    newBibtex: string
+    isCounterOffer: boolean
+    requestTagProjects: RequestTagProject[]
+    requestMediaProjects: RequestMediaProject[]
+    requestLinkProjects: RequestLinkProject[]
+    requestCollaboratorsProjects: RequestCollaboratorsProjects[]
+}
+
+export interface RequestCollaboratorsProjects {
+    id: string
+    isRemove: boolean
+}
+
+export interface RequestTagProject {
+    requestTagProjectID: string
+    isRemove: boolean
+
+}
+
+export interface Tag {
+    tagId: string
+    name: string
+    color: string
+    requestTagProjects: RequestTagProject[]
+    tagsToProjects: TagToProject[]
+}
+
+
