7d0e7aafe0e9c6ad1dd22201bafe5bad9158debd
Filip-Andrei Cirtog <f.a.cirtog@student.tudelft.nl>
2024-05-26 20:23:56 +0200

    Project-detail-webpage

--- src/app/features/projects/components/project-detail/project-detail.component.ts
+++ src/app/features/projects/components/project-detail/project-detail.component.ts
@@ -1,30 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import { AccordionModule } from 'primeng/accordion';
-import { BadgeModule } from 'primeng/badge';
-import { AvatarModule } from 'primeng/avatar';
-import { CardModule } from 'primeng/card';
-import { SplitterModule } from 'primeng/splitter';
-import { CommonModule } from '@angular/common';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA ,NO_ERRORS_SCHEMA} from '@angular/core';
+import {Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, OnInit} from '@angular/core';
+import {AccordionModule} from 'primeng/accordion';
+import {BadgeModule} from 'primeng/badge';
+import {AvatarModule} from 'primeng/avatar';
+import {CardModule} from 'primeng/card';
+import {SplitterModule} from 'primeng/splitter';
+import {CommonModule} from '@angular/common';
 import {TagModule} from 'primeng/tag';
 import {ButtonModule} from 'primeng/button';
 import {CarouselModule} from 'primeng/carousel';
-import { ProjectService } from '../../services/project-detail.service';
-import { ChipModule } from 'primeng/chip';
+import {ProjectService} from '../../services/project-detail.service';
+import {ChipModule} from 'primeng/chip';
 import {OrderListModule} from 'primeng/orderlist';
-import {Collaborator, Link, Media, MediaFile, Project, Tag} from "../../models/project-models";
-import {ActivatedRoute, Router} from '@angular/router';
+import {Collaborator, Link, MediaFile, Project, Tag} from "../../models/project-models";
+import {ActivatedRoute} from '@angular/router';
+
 @Component({
   selector: 'app-project-detail',
   standalone: true,
-  imports: [ChipModule,AccordionModule, BadgeModule, AvatarModule, CardModule, SplitterModule, CommonModule,TagModule,ButtonModule,CarouselModule,OrderListModule],
+  imports: [ChipModule, AccordionModule, BadgeModule, AvatarModule, CardModule, SplitterModule, CommonModule, TagModule, ButtonModule, CarouselModule, OrderListModule],
   templateUrl: './project-detail.component.html',
-  schemas: [CUSTOM_ELEMENTS_SCHEMA,NO_ERRORS_SCHEMA],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA],
   styleUrls: ['./project-detail.component.css']
 })
-export class ProjectDetailComponent implements OnInit{
+export class ProjectDetailComponent implements OnInit {
   imageFiles: MediaFile[] = [];
   otherFiles: MediaFile[] = [];
+  bibTeX: MediaFile | undefined = {
+    a: '',
+    b: '',
+    c: ''
+  };
   projectId: string = "";
   project: Project = {
     projectId: '',
@@ -41,25 +46,28 @@ export class ProjectDetailComponent implements OnInit{
   };
   collaborators: Collaborator[] = [];
   links: Link[] = [];
-  tags: Tag[] =[];
+  tags: Tag[] = [];
+  responsiveOptions: any[] | undefined;
+  private mimeTypes: { [key: string]: string } = {
+    'jpg': 'image/jpeg',
+    'jpeg': 'image/jpeg',
+    'png': 'image/png',
+    'gif': 'image/gif',
+    'bmp': 'image/bmp',
+    'webp': 'image/webp',
+    'pdf': 'application/pdf',
+    'txt': 'text/plain',
+    'html': 'text/html',
+    'bib': 'application/x-bibtex'
+  };
 
-  constructor(private readonly projectService: ProjectService,private route: ActivatedRoute) { }
+  constructor(private readonly projectService: ProjectService, private route: ActivatedRoute) {
+  }
+
   getCollaboratorNames(): string {
     return this.collaborators.map(obj => obj.name).join(', ');
   }
-  getBibTex(): string {
-    return "@Article { MHCOLHHASD24,\n" +
-      "  author       = \"Mody, Prerak P. and Huiskes, Merle  and Chaves de Plaza, Nicolas and Onderwater, Alice and Lamsma, Rense and\n" +
-      "                  Hildebrandt, Klaus and Hoekstra, Nienke and Astreinidou, Eleftheria and Staring, Marius and Dankers, Frank\",\n" +
-      "  title        = \"\tLarge-scale dose evaluation of deep learning organ contours in head-and-neck radiotherapy by leveraging\n" +
-      "                  existing plans\",\n" +
-      "  journal      = \"Physics and Imaging in Radiation Oncology\",\n" +
-      "  year         = \"2024\",\n" +
-      "  doi          = \"https://doi.org/10.1016/j.phro.2024.100572\",\n" +
-      "  url          = \"http://graphics.tudelft.nl/Publications-new/2024/MHCOLHHASD24\"\n" +
-      "}";
-  }
-  responsiveOptions: any[] | undefined;
+
   ngOnInit() {
     this.route.params.subscribe(params => {
       this.projectId = (params['id']);
@@ -71,6 +79,7 @@ export class ProjectDetailComponent implements OnInit{
       });
       this.projectService.getCollaboratorsByProjectId(params['id']).subscribe(responseCollaborators => {
         this.collaborators = responseCollaborators;
+        console.log(this.collaborators);
       });
       this.projectService.getTagsByProjectId(params['id']).subscribe(responseTags => {
         this.tags = responseTags;
@@ -78,12 +87,12 @@ export class ProjectDetailComponent implements OnInit{
     });
     this.projectService.getProjectMedia(this.projectId).subscribe({
       next: (data: MediaFile[]) => {
-        this.imageFiles = data.filter(media => {
-          return media.a && (media.a.endsWith(".jpg") || media.a.endsWith(".png"));
-        });
-        this.otherFiles = data.filter(media => {
-          return media.a && !(media.a.endsWith(".jpg") || media.a.endsWith(".png"));
-        });
+        this.imageFiles = data.filter(media => media.a && (media.a.endsWith(".jpg") || media.a.endsWith(".png")));
+        this.otherFiles = data.filter(media => media.a && !(
+          media.a.endsWith(".jpg") ||
+          media.a.endsWith(".png")
+        ));
+        this.bibTeX = data.find(media => media.a && media.a.endsWith(".bib"));
       },
       error: (err) => {
         console.error('Error fetching media files', err);
@@ -112,26 +121,16 @@ export class ProjectDetailComponent implements OnInit{
   getImageSrc(media: MediaFile): string {
     return `data:${media.a};base64,${media.b}`;
   }
-  private mimeTypes: { [key: string]: string } = {
-    'jpg': 'image/jpeg',
-    'jpeg': 'image/jpeg',
-    'png': 'image/png',
-    'gif': 'image/gif',
-    'bmp': 'image/bmp',
-    'webp': 'image/webp',
-    'pdf': 'application/pdf',
-    'txt': 'text/plain',
-    'html': 'text/html',
-  };
 
   getMimeType(fileName: string): string {
     const extension = fileName.split('.').pop()?.toLowerCase() || '';
     return this.mimeTypes[extension] || 'application/octet-stream';
   }
+
   downloadFile(media: MediaFile) {
     const mimeType = this.getMimeType(media.a)
     const byteArray = new Uint8Array(atob(media.b).split('').map(char => char.charCodeAt(0)));
-    const file = new Blob([byteArray], { type: mimeType });
+    const file = new Blob([byteArray], {type: mimeType});
     const fileUrl = URL.createObjectURL(file);
     const fileName = media.a;
     let link = document.createElement("a");
@@ -142,8 +141,36 @@ export class ProjectDetailComponent implements OnInit{
     document.body.removeChild(link);
     URL.revokeObjectURL(fileUrl);
   }
+
   isAbsoluteUrl(url: string): boolean {
     return url.startsWith('http://') || url.startsWith('https://');
   }
-  protected readonly document = document;
+
+  convertBibtex(): string | undefined {
+    if (this.bibTeX == undefined) {
+      return undefined;
+    }
+    const decodedStr = atob(this.bibTeX.b);
+    const utf8Str = decodeURIComponent(escape(decodedStr));
+    const lines = utf8Str.split('\n');
+    let maxIndex = 0;
+    const parts = lines.map(line => {
+      const index = line.indexOf('=');
+      if (index > maxIndex) {
+        maxIndex = index;
+      }
+      return {line, index};
+    });
+    const formattedLines = parts.map(part => {
+      if (part.index === -1) {
+        return part.line;
+      }
+      const padding = ' '.repeat(maxIndex - part.index);
+      return part.line.replace('=', `${padding} =`);
+    });
+    const formattedBibtex = formattedLines.map(line => {
+      return line.replace(/\{(.*?)\}/g, '"$1"');
+    });
+    return formattedBibtex.join('\n');
+  }
 }
